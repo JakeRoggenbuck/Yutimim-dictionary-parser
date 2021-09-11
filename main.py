ce972f39f59ee2672375af6c47cb90237441149f
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-09-10 17:43:26 -0700

    Add outfile and verbose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from plrs import Lexer, is_part_numeric
 from dataclasses import dataclass
 from typing import List
+import argparse
+import json
 
 
 @dataclass
@@ -12,34 +14,60 @@ class Word:
         return {"word": self.word, "defs": self.defs}
 
 
-with open("./dict.txt") as file:
-    lines = file.readlines()
-
+def parse(filename: str):
     words = []
-    current_word = None
 
-    for line in lines:
-        # Check for close to empty lines
-        if len(line) < 2:
-            continue
+    with open(filename) as file:
+        lines = file.readlines()
 
-        parts = line.split()
+        current_word = None
 
-        # Check if the first part is not enumerated, like ['1.', 'that']
-        # parts[0] is '1.' and is_part_numeric('1.') is True
-        if not is_part_numeric(parts[0]):
-            # Add the last word to the words list
-            if current_word is not None:
-                words.append(current_word)
-            # Make a new word with empty defs
-            current_word = Word(parts[0], [])
-        else:
-            # Add a definition to defs
-            current_word.defs.append(" ".join(parts[1:]))
+        for line in lines:
+            # Check for close to empty lines
+            if len(line) < 2:
+                continue
 
-    # Add the last word because it's not caught
-    words.append(current_word)
+            parts = line.split()
 
-    for word in words:
-        # print(dict(word))
-        print(word.__dict__())
+            # Check if the first part is not enumerated, like ['1.', 'that']
+            # parts[0] is '1.' and is_part_numeric('1.') is True
+            if not is_part_numeric(parts[0]):
+                # Add the last word to the words list
+                if current_word is not None:
+                    words.append(current_word)
+                # Make a new word with empty defs
+                current_word = Word(parts[0], [])
+            else:
+                # Add a definition to defs
+                current_word.defs.append(" ".join(parts[1:]))
+
+        # Add the last word because it's not caught
+        words.append(current_word)
+
+
+    return words
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--infile", help="Input filename", required=True)
+    parser.add_argument("-o", "--outfile", help="Output filename")
+    parser.add_argument("-v", "--verbose", help="Verbose", action="store_true")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    words = parse(args.infile)
+
+    # Print all the words
+    if args.verbose:
+        for word in words:
+            print(word.__dict__())
+
+    # Write the output to json file
+    if args.outfile:
+        with open(args.outfile, "w") as file:
+            json.dump([x.__dict__() for x in words], file)
+        print(f"Wrote json to {args.outfile}!")
