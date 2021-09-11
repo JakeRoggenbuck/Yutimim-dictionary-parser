5897cc805aaa3d511b8b7f2b730f5322ad20e816
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-09-10 17:05:08 -0700

    Make parser

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from plrs import Lexer, is_part_numeric
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Word:
+    word: str
+    defs: List[str]
+
 
 with open("./dict.txt") as file:
     lines = file.readlines()
 
+    words = []
+    current_word = None
+
     for line in lines:
-        print(line)
+        if len(line) < 2:
+            continue
+
+        parts = line.split()
+
+        if not is_part_numeric(parts[0]):
+            words.append(current_word)
+            current_word = Word(parts[0], [])
+        else:
+            current_word.defs.append(" ".join(parts[1:]))
+
+    words.append(current_word)
+
+    for word in words:
+        print(word)
